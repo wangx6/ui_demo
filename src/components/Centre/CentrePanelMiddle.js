77c313fb64a12a946764bd2c6336e3cf34db936b
Eamonn Walsh <eamonnprwalsh@gmail.com>
2021-03-10 15:19:27 +0000

    Quote Form

--- src/components/Centre/CentrePanelMiddle.js
+++ src/components/Centre/CentrePanelMiddle.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './CentrePanel.css';
+import QuoteForm from '../Forms/QuoteForm';
 
-export default function CentrePanelMiddle() {
+export default function CentrePanelMiddle(){
 
+const formRecord = {
+    inputs: { 
+        firstName: 'Eamonn',
+        lastName: '',
+        dob: '',
+        term: '',
+        email: ''
+    },
+    errors: {
+
+    }
+}
     return (
         <div className="lqe-centre-panel__middle"> 
         <h3>HI2</h3>
+        <QuoteForm formRecord={formRecord} ></QuoteForm>
         </div>
     )
 }
