cb9480c6db34e16250ebfc9449bb33d9b80f1fbf
ashishbharvirkar <ashishbharvirkar@yahoo.com>
2021-03-10 18:12:26 +0530

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,15 @@ import './Header.css';
 export default function Header() {
     return (
         <div className="lqe-hdr">
-            Header
+            <div className='lqe-top-navbar-header'>
+                <div className="lqe-left-top-navbar-sec">
+                LIFE DEMO
+                </div>
+                <div className="lqe-right-top-navbar-sec">
+                   <div className="lqe-left-sec"><a className="lqe-phone-link" href="tel:+1-51111-111">051111-111</a></div>
+                   <div className="lqe-right-sec"><button className="lqe-my-life-button" type="button"></button></div>
+                </div>
+            </div>
         </div>
     )
 }
