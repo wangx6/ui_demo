11d7145475aa574b0015562b57f599f805a9160a
Martin O'Sullivan <mosabcdef@yahoo.com>
2021-03-10 16:35:48 +0000

    Adding quote Service and Models

---
+++ src/models/Quote/Quote.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+function Quote() {
+
+    const formRecord = {
+        inputs: { 
+            firstName: 'Eamonn',
+            lastName: '',
+            dob: '',
+            term: '',
+            email: ''
+        },
+        errors: {
+
+        }
+    }
+
+
+    // power
+    function getFormRecord() {
+        return formRecord;
+    }
+
+    function setFormRecord(record) {
+        formRecord.inputs = Object.assign({}, record);
+    }
+
+    async function submitQuote() {
+        console.log('Quote.js - ' + formRecord.inputs);
+        const result = await axios.post('http://localhost:8080/quote', formRecord.inputs);
+        console.log(result);
+        return result;
+    }
+
+    // call api
+
+    return {submitQuote, getFormRecord, setFormRecord};
+}
+
+export default Quote;
