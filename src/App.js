c1eec1e6d91c0663cc7040d0c099d3baef3f0429
Eamonn Walsh <eamonnprwalsh@gmail.com>
2021-03-10 11:01:03 +0000

    commit message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import CentrePanel from './components/Centre/CentrePanel';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <CentrePanel>
+      </CentrePanel>
     </div>
   );
 }
