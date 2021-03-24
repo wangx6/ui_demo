951f7a7b9aef8f07a426b7da4ee105344bc1c162
Martin O'Sullivan <mosabcdef@yahoo.com>
2021-03-24 11:41:24 +0000

    Updates as per code review

--- src/components/CookieBanner/CookieBanner.js
+++ src/components/CookieBanner/CookieBanner.js
@@ -7,29 +7,29 @@ export default function CookieBanner() {
 
   const [isVisible, setIsVisible] = useState(false);
 
-  function ShowCookieBanner() {
+  function onShowCookieBanner() {
     setIsVisible(true);
   }
 
-  function hideCookieBanner() {
+  function onHideCookieBanner() {
     setIsVisible(false);
   }
 
   return (
     <div className='util-cookie-banner'>
-      <div className={`util-cookie-banner-container util-cookie-banner-container--${isVisible ? "show" : "hide"}`}>
+      <div className={`util-cookie-banner-container util-cookie-banner-container--${isVisible ? "visible" : "invisible"}`}>
         <div className='util-cookie-banner-container-hdr'>
           <img src={VhiLogo} width='120' height='40' alt='Vhi Logo' />
-          <CgClose className='util-cookie-banner__cookie-icon-hide' onClick={hideCookieBanner}></CgClose>
+          <CgClose className='util-cookie-banner__cookie-icon-hide' onClick={onHideCookieBanner}></CgClose>
         </div>
         <div className='util-cookie-banner-container-content'>
           <hr />Cookie Banner Content
                 </div>
       </div>
       <div className='util-cookie-banner__cookie-icon'>
-        <img className='util-cookie-banner__cookie-icon-show' onClick={ShowCookieBanner} src={CookieIcon} width='35' height='35' alt='Cookie Icon' />
+        <img className='util-cookie-banner__cookie-icon-show' onClick={onShowCookieBanner} src={CookieIcon} alt='Cookie Icon' />
       </div>
-      <div className={`util-cookie-banner__modal ${isVisible ? "show" : "hide"}`}></div>
+      <div className={`util-cookie-banner__modal ${isVisible ? "visible" : "invisible"}`}></div>
     </div>
   )
 }
